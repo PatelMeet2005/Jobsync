70a7a4d765ef4b1b5ca54b00c7ba4fadd84a88a7
PatelMeet2005 <meetpatel011005@gmail.com>
2025-07-09 14:07:34 +0530

    login

--- jobsyncfrontend/src/Component/Login/Login.jsx
+++ jobsyncfrontend/src/Component/Login/Login.jsx
@@ -8,7 +8,14 @@ const Login = ( { onClose }) => {
         <button className="close-btn" onClick={onClose}>×</button>
         <div className="heading">
           <h2>Sign In</h2>
-          
+          <p className='heading-p'>Sign in to your Account.</p>
+          <div className="signup">
+            <p>Don't have an account?</p>
+            <a href="#" className="google-btn">Sign up</a>           
+          </div>
+          <form action="">
+            <label htmlFor=""></label>
+          </form>
         </div>
       </div>
     </div>
