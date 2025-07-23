719eeecd62a265ec973b691a199d4070de24574f
Manav patel <2006manavpatel@.com>
2025-07-23 13:49:50 +0530

    messeage

---
+++ jobsyncbackend/JobSync.API/Settings/JobSyncDatabaseSettings.cs
@@ -0,0 +1,8 @@
+namespace JobSync.API.Settings
+{
+    public class JobSyncDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+    }
+}
